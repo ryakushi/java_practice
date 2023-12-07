43bc7cb13104905c11ed292ccad625cc1124e667
Your Name <you@example.com>
2023-12-07 16:41:12 +0900

    挿入ソートの練習

--- java9/Main.java
+++ java9/Main.java
@@ -2,5 +2,30 @@ package java9;
 
 public class Main {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    int[] data = { 30, 60, 70, 90, 20, 10,  };
+    for (int element : data) {
+      System.out.print(element + " ");
+    }
+    System.out.println();
+
+    sort(data);
+
+    System.out.println();
+    for (int element : data) {
+      System.out.print(element + " ");
+    }
+  }
+
+  private static void sort(int[] data) {
+    for (int i = 1; i < data.length; i++) {
+      int tmp = data[i];
+      int j = i;
+      while (j > 0 && tmp < data[j - 1]) {
+        data[j] = data[j - 1];
+        j--;
+      }
+      data[j] = tmp;
+    }
+  }
 }
