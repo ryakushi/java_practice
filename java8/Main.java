1efa2633142ceb34febe64a4c30e3a9c9f2f7155
Your Name <you@example.com>
2023-12-07 16:23:32 +0900

    コメントの追加

--- java8/Main.java
+++ java8/Main.java
@@ -8,7 +8,9 @@ public class Main {
       System.out.print(i + " ");
     }
     System.out.println();
+
     sort(data);
+
     System.out.println();
     for (int element : data) {
       System.out.print(element + " ");
@@ -17,13 +19,14 @@ public class Main {
 
   public static void sort(int[] data) {
     for (int i = 0; i < data.length; i++) {
-      int min = i;
-      for (int j = i + 1; j < data.length; j++) {
+      int min = i; //最小の要素を[ i ]の0番目からスタート
+      for (int j = i + 1; j < data.length; j++) { // jの初期値はi + 1でとなりの要素からスタート
+        //変数minよりも小さい値があればminを更新
         if (data[j] < data[min]) {
           min = j;
         }
       }
-
+      //見つけた新しい最小の数値と既存の数値を入れ替え
       int tmp = data[i];
       data[i] = data[min];
       data[min] = tmp;
