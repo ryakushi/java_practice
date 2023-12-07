5ccae0c919afc1f88db2078cd5214459a8ac7f00
Your Name <you@example.com>
2023-12-07 16:15:49 +0900

    選択ソートの練習

--- java8/Main.java
+++ java8/Main.java
@@ -2,5 +2,31 @@ package java8;
 
 public class Main {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    int[] data = { 30, 60, 70, 90, 20, 100 };
+    for (int i : data) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    sort(data);
+    System.out.println();
+    for (int element : data) {
+      System.out.print(element + " ");
+    }
+  }
+
+  public static void sort(int[] data) {
+    for (int i = 0; i < data.length; i++) {
+      int min = i;
+      for (int j = i + 1; j < data.length; j++) {
+        if (data[j] < data[min]) {
+          min = j;
+        }
+      }
+
+      int tmp = data[i];
+      data[i] = data[min];
+      data[min] = tmp;
+    }
+  }
 }
