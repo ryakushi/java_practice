3aa2a31355e31efa5ccf67e9bf0c60cbbb85cfd0
Your Name <you@example.com>
2023-12-07 15:07:24 +0900

    最大値の問題の解答

---
+++ java4/Main.java
@@ -0,0 +1,24 @@
+package java4;
+
+/**
+ * Main
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    int[] arrayNum = { 30, 50, 20, 60, 80, 90, 20 };
+    System.out.println(max(arrayNum));
+  }
+
+  static int max(int[] data) {
+    int max = data[0]; //最大値の初期値を配列の最初の値からスタートする
+    for (int i = 1; i < data.length; i++) { //変数maxと比較するため i のインデックス番号を1からスタート
+      if (max < data[i]) {
+        // 変数maxよりdata[i]が大きければ更新
+        max = data[i];
+      }
+    }
+
+    return max;
+  }
+}
