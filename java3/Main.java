cfc0d7fc1ccfbcfb86deb0de38e038d2b8428456
Your Name <you@example.com>
2023-12-07 10:51:58 +0900

    基本情報 科目 B アルゴリズムとプログラミング サンプル問題3

---
+++ java3/Main.java
@@ -0,0 +1,20 @@
+package java3;
+
+import java.util.Arrays;
+
+public class Main {
+
+  public static void main(String[] args) {
+    int[] array = { 0, 1, 2, 3, 4, 5 }; // Java配列が0始まりのためダミーの0を追加
+    int right, left;
+    int tmp;
+
+    for (left = 1; left < (array.length / 2); left += 1) {
+      right = array.length - left;
+      tmp = array[right];
+      array[right] = array[left];
+      array[left] = tmp;
+    }
+    System.out.println(Arrays.toString(array));
+  }
+}
