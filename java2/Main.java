fdcd09cda224ecbd4f79b122f80e47744f051b17
Your Name <you@example.com>
2023-12-07 10:28:10 +0900

    基本情報 科目 B アルゴリズムとプログラミング サンプル問題2の解答

--- java2/Main.java
+++ java2/Main.java
@@ -1,8 +1,18 @@
-package java2;
-
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("hello");
+    System.out.println(fee(-1));
+  }
+
+  static int fee(int age) {
+    int ret;
+    if (age <= 3) {
+      return 100;
+    } else if (age <= 9) {
+      return 300;
+    } else {
+      return 500;
+    }
+    return ret;
   }
 }
